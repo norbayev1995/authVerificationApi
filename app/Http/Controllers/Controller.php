29497794a98e87734950c408dae9e0ed9e6ad754
Mansur <ttytkhkttymi@gmail.com>
2025-03-08 04:40:52 +0500

    Added uploadPhoto method in abstract Controller for file handling

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,9 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    public function uploadPhoto($file, $path = 'uploads')
+    {
+        $photoName = md5(time() . $file->getClientOriginalName()) . '.' . $file->getClientOriginalExtension();
+        return $file->storeAs($path, $photoName, 'public');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,11 +6,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -21,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'avatar',
     ];
 
     /**
@@ -44,5 +46,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function setPasswordAttribute($password)
+    {
+        $this->attributes['password'] = Hash::make($password);
     }
 }
