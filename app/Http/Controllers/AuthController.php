411bed4efad9e03f2ea527cba5164961db074300
Mansur <ttytkhkttymi@gmail.com>
2025-03-09 12:07:48 +0500

    Added user registration with email verification token and avatar upload

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,8 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->verification_token = Str::random(64);
+        if ($request->hasFile('avatar')) {
+            $user->avatar = $this->uploadPhoto($request->file('avatar'), 'avatars');
+        }
+        $user->save();
+        return response()->json([
+            'message' => 'User created successfully',
+            'user' => $user
+        ], 201);
+    }
 }
